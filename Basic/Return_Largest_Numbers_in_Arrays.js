d5ba4d7241ca5fc04d7efcb4fa29654cf63eb970
YORY31 <20230272@itla.edu.do>
2025-03-20 18:20:39 -0400

    first commit

---
+++ Basic/Return_Largest_Numbers_in_Arrays.js
@@ -0,0 +1,9 @@
+function largestOfFou(arr){
+    const results = [];//empty array for storing larger numbers 
+
+    for (let i = 0; i <arr.length; i++){
+        let largestNum = arr[i][0];//set the first number in the array as the largest number
+
+    }
+
+}
